4a8c2cedf1fb51921b61656e2a80404257fd2579
Sasha Voitovych <s.voitovychh@gmail.com>
2020-12-01 21:52:52 -0500

    Completed profile driver implementation. As song microservice is not yet implemented, getAllSongsFriendsLike returns json with song ids instead of song names

--- profile-microservice/src/main/java/com/csc301/profilemicroservice/ProfileDriverImpl.java
+++ profile-microservice/src/main/java/com/csc301/profilemicroservice/ProfileDriverImpl.java
@@ -12,6 +12,8 @@ import org.neo4j.driver.v1.StatementResult;
 
 import org.springframework.stereotype.Repository;
 import org.neo4j.driver.v1.Transaction;
+import static org.neo4j.driver.v1.Values.parameters;
+
 
 @Repository
 public class ProfileDriverImpl implements ProfileDriver {
@@ -41,35 +43,113 @@ public class ProfileDriverImpl implements ProfileDriver {
 	@Override
 	public DbQueryStatus createUserProfile(String userName, String fullName, String password) {
 		
-		DbQueryStatus mock = new DbQueryStatus("this is my mock message", DbQueryExecResult.QUERY_OK);
-		mock.setData("this is my mock data");
-		return mock;
+		DbQueryExecResult ifSuccessful;
+		try (Session session = ProfileMicroserviceApplication.driver.session()) {
+			try (Transaction trans = session.beginTransaction()) {
+				String queryStr = "CREATE (nProfile:profile {userName: $userName, fullName: $fullName, password: $password})\n"
+						+ "CREATE (nPlaylist:playlist {plName: $userName + \"-favourites\"})\n"
+						+ "CREATE (nProfile)-[:created]->(nPlaylist)";
+				trans.run(queryStr, parameters("userName", userName, "fullName", fullName, "password", password));
+				trans.success();
+				ifSuccessful = DbQueryExecResult.QUERY_OK;
+			}catch(Exception e) {
+				ifSuccessful = DbQueryExecResult.QUERY_ERROR_GENERIC;
+			}
+			session.close();
+		}
+		
+		DbQueryStatus status = new DbQueryStatus("create user profile", ifSuccessful);
+		return status;
 		
 	}
 
 	@Override
 	public DbQueryStatus followFriend(String userName, String frndUserName) {
 		
-		DbQueryStatus mock = new DbQueryStatus("this is my mock message", DbQueryExecResult.QUERY_OK);
-		mock.setData("this is my mock data");
-		return mock;
+		DbQueryExecResult ifSuccessful;
+		try (Session session = ProfileMicroserviceApplication.driver.session()) {
+			try (Transaction trans = session.beginTransaction()) {
+				String queryStr = "MATCH(user:profile {userName: $userName})\n"
+						+ "MATCH(friend:profile {userName: $frndUserName})\n"
+						+ "CREATE(user)-[:follows]->(friend)\n"
+						+ "RETURN COUNT(user) as userCount, COUNT(friend) as friendCount";
+				StatementResult res = trans.run(queryStr, parameters("userName", userName, "frndUserName", frndUserName));
+				
+				Record rec = res.next();
+				boolean not404 = (long)rec.asMap().get("userCount") > 0 && (long)rec.asMap().get("friendCount") > 0;
+				trans.success();
+				
+				ifSuccessful = not404 ? DbQueryExecResult.QUERY_OK : DbQueryExecResult.QUERY_ERROR_NOT_FOUND;
+			}catch(Exception e) {
+				ifSuccessful = DbQueryExecResult.QUERY_ERROR_GENERIC;
+			}
+			session.close();
+		}
+		
+		DbQueryStatus status = new DbQueryStatus("follow a friend", ifSuccessful);
+		return status;
 		
 	}
 
 	@Override
 	public DbQueryStatus unfollowFriend(String userName, String frndUserName) {
 		
-		DbQueryStatus mock = new DbQueryStatus("this is my mock message", DbQueryExecResult.QUERY_OK);
-		mock.setData("this is my mock data");
-		return mock;
+		DbQueryExecResult ifSuccessful;
+		try (Session session = ProfileMicroserviceApplication.driver.session()) {
+			try (Transaction trans = session.beginTransaction()) {
+				String queryStr = "MATCH(user:profile {userName: $userName})\n"
+						+ "MATCH(friend:profile {userName: $frndUserName})\n"
+						+ "MATCH(user)-[f:follows]->(friend)\n"
+						+ "DELETE f \n"
+						+ "RETURN COUNT(user) as userCount, COUNT(friend) as friendCount";
+				StatementResult res = trans.run(queryStr, parameters("userName", userName, "frndUserName", frndUserName));
+				
+				Record rec = res.next();
+				boolean not404 = (long)rec.asMap().get("userCount") > 0 && (long)rec.asMap().get("friendCount") > 0;
+				trans.success();
+				
+				ifSuccessful = not404 ? DbQueryExecResult.QUERY_OK : DbQueryExecResult.QUERY_ERROR_NOT_FOUND;
+			}catch(Exception e) {
+				ifSuccessful = DbQueryExecResult.QUERY_ERROR_GENERIC;
+			}
+			session.close();
+		}
+		
+		DbQueryStatus status = new DbQueryStatus("follow a friend", ifSuccessful);
+		return status;
+		
 	}
 
 	@Override
 	public DbQueryStatus getAllSongFriendsLike(String userName) {
 			
-		DbQueryStatus mock = new DbQueryStatus("this is my mock message", DbQueryExecResult.QUERY_OK);
-		mock.setData("this is my mock data");
-		return mock;
+		DbQueryExecResult ifSuccessful;
+		Object data;
+		try (Session session = ProfileMicroserviceApplication.driver.session()) {
+			try (Transaction trans = session.beginTransaction()) {
+				String queryStr = "MATCH (:profile {userName: $userName})-[:follows]->(friend:profile)\n"
+						+ "OPTIONAL MATCH (friend)-[:created]->(list:playlist {plName: friend.userName + \"-favourites\"})-[:contains]->(s: song)\n"
+						+ "WITH friend.userName as name, COLLECT(s.songId) as songs\n"
+						+ "RETURN COLLECT([name, songs]) as pairs";
+				StatementResult res = trans.run(queryStr, parameters("userName", userName));
+				
+				if (res.hasNext()) {
+					data = res.next().asMap().get("pairs");
+				}else {
+					data = null;
+				}
+				
+				ifSuccessful = DbQueryExecResult.QUERY_OK;
+			}catch(Exception e) {
+				ifSuccessful = DbQueryExecResult.QUERY_ERROR_GENERIC;
+				data = null;
+			}
+			session.close();
+		}
+		
+		DbQueryStatus status = new DbQueryStatus("follow a friend", ifSuccessful);
+		status.setData(data);
+		return status;
 		
 	}
 }
